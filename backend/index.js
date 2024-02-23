e214476500150f289eca7f8b61c7095bab3c2f00
Shree1805yadav <nainay1805@gmail.com>
2024-02-23 23:07:10 +0530

    Connected to DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log(`Connected to MongoDB`);
+}).catch((err) => {
+    console.log(`Error in MongoDB`, err);
+});
 
 const app = express();
 
-const PORT = 3000;
+// Fetch port from environment variable or use default value 3000
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello, world!"); // example 
